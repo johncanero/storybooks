e1f1e1627fd324edffd46191beff367747645ded
John Canero <johncanero.official@gmail.com>
2022-08-18 16:08:56 +0800

    Input a console.log in app.listen = node.env and port

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ dotenv.config({ path: './config/config.ev' })
 
 const app = express();
 
+const PORT = process.env.port || 3000
 
-app.listen9
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
 
 
 
