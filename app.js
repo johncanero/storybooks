eb459cd6f7d30925e3c3f73b6ae8516718e9aca4
John Canero <johncanero.official@gmail.com>
2022-08-18 19:40:46 +0800

    handlebars, handlebars helpers code and create helpers folder

--- app.js
+++ app.js
@@ -1,15 +1,55 @@
 const express = require('express');
-const dotenv = require("dotenv");
-const connectDB = require('./config/db')
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const connectDB = require('./config/db');
+
 
 // Load config
 dotenv.config({ path: './config/config.env' })
 
+// connectDB function
 connectDB();
 
 
 // 1. Create const app = express()
 const app = express();
+
+// morgan console
+if (process.env.NODE_ENV == 'development') {
+    app.use(morgan('dev'));
+};
+
+
+
+// Handlebars Helpers
+const {
+    formatDate,
+    stripTags,
+    truncate,
+    editIcon,
+    select,
+  } = require('./helpers/hbs')
+  
+  // Handlebars
+  app.engine(
+    '.hbs',
+    exphbs({
+      helpers: {
+        formatDate,
+        stripTags,
+        truncate,
+        editIcon,
+        select,
+      },
+      defaultLayout: 'main',
+      extname: '.hbs',
+    })
+  )
+  app.set('view engine', '.hbs')
+  
+
+
 
 
 // 2. Create PORT 3000
