436d38767bb7c5ec9e31e26b4b0c0693e6c32eb0
John Canero <johncanero.official@gmail.com>
2022-08-19 00:39:39 +0800

    Google Authentication Passport and User Schema Commit

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
+const passport = require('passport');
+const session = require('express-session');
 const connectDB = require('./config/db');
 
 
 // Load config
-dotenv.config({ path: './config/config.env' })
+dotenv.config({ path: './config/config.env' });
+
+// Passport config
+require('./config/passport')(passport);
+
 
 // connectDB function
 connectDB();
@@ -24,6 +30,19 @@ if (process.env.NODE_ENV == 'development') {
 // Create Handlebars to set hbs files
 app.engine('.hbs', exphbs.engine({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
+
+
+// Sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+  }))
+
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Create a static folder for public and css
 app.use(express.static(path.join(__dirname, 'public')))
