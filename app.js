8100e9e857bc1b052b39be72651b83c908401e84
John Canero <johncanero.official@gmail.com>
2022-08-18 16:06:26 +0800

    Create dotenv config, const app, app.listen and config.env file

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+// Load config
+dotenv.config({ path: './config/config.ev' })
+
+const app = express();
+
+
+app.listen9
+
+
+
