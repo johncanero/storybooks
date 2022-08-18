91d1002d02cef84ba63b48d4c8553644d33b86cb
John Canero <johncanero.official@gmail.com>
2022-08-18 19:07:15 +0800

    Add comments in app.js and create a file called db.js

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const dotenv = require('dotenv');
 // Load config
 dotenv.config({ path: './config/config.ev' })
 
+// 1. Create const app = express()
 const app = express();
 
+
+// 2. Create PORT 3000
 const PORT = process.env.port || 3000
 
-
+// 3 Create app.listen
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
 
 
