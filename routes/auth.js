a2b035c89636d7bd53f8995a402f28b664e38ea6
John Canero <johncanero.official@gmail.com>
2022-08-19 20:17:46 +0800

    Fix logout to return home page, make function sidenav, header styling

--- routes/auth.js
+++ routes/auth.js
@@ -11,13 +11,23 @@ router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
 // @description: Google auth callback
 // @route: GET /auth/google/callback
 router.get(
-    '/google/callback', 
-    passport.authenticate('google', { failureRedirect: '/' }), 
-    (req, res) => {
-    res.redirect('/dashboard');
-}) 
+        '/google/callback', 
+        passport.authenticate('google', { failureRedirect: '/' }), 
+        (req, res) => {
+        res.redirect('/dashboard');
+    }
+);
 
 
+
+// @description: Logout user
+// @route: GET /auth/logout
+router.get('/logout', (req, res, next) => {
+        req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+      });
+});
 
 
 module.exports = router;
