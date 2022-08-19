d1ed6b9b4178d7bb148685233e012b53809cee86
John Canero <johncanero.official@gmail.com>
2022-08-19 22:47:17 +0800

    Add story & show stories

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { ensureAuth, ensureGuest } = require('../middleware/auth');
+const Story = require('../models/Story');
 
 
 // @description: Login/Landing Page
@@ -15,9 +16,16 @@ router.get('/', ensureGuest, (req, res) => {
 // @description: Dashboard
 // @route: GET /dashboard
 router.get('/dashboard', ensureAuth, (req, res) => {  
-    res.render('dashboard', {
+
+    try {
+      const stories = await Story.find({ user: req.user.id }).lean()    
+      res.render('dashboard', {
         name: req.user.firstName,
-    })
+        stories
+    }) 
+    } catch (err) {
+        console.error(err);
+    }
 });
 
 
