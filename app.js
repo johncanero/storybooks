96eafa528a1d9bc40c2aeb0cabcd81e89b8fa578
John Canero <johncanero.official@gmail.com>
2022-08-20 08:18:41 +0800

    Require stories route and add javascript to function: private and public options

--- app.js
+++ app.js
@@ -55,6 +55,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Routes
 app.use('/', require('./routes/index'));
 app.use('/auth', require('./routes/auth'));
+app.use('/stories', require('./routes/stories'));
 
 
 // 2. Create PORT 3000
